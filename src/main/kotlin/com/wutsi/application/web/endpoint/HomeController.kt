6bed5309e6ec9d552a01d48465548216bd8da9cb
wutsi <herve.tchepannou@wutsi.com>
2022-02-08 17:51:51 -0500

    add home page

--- src/main/kotlin/com/wutsi/application/web/endpoint/HomeController.kt
+++ src/main/kotlin/com/wutsi/application/web/endpoint/HomeController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-@RequestMapping("/product")
+@RequestMapping("/")
 class HomeController(
     private val catalogApi: WutsiCatalogApi,
 
