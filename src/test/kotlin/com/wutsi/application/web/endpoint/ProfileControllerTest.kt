2201077ea8a7be673db57f4b709d4a57f1c6f504
wutsi <herve.tchepannou@wutsi.com>
2022-02-15 07:02:12 -0500

    improve profile

--- src/test/kotlin/com/wutsi/application/web/endpoint/ProfileControllerTest.kt
+++ src/test/kotlin/com/wutsi/application/web/endpoint/ProfileControllerTest.kt
@@ -6,24 +6,26 @@ import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.platform.account.WutsiAccountApi
 import com.wutsi.platform.account.dto.Account
 import com.wutsi.platform.account.dto.GetAccountResponse
+import com.wutsi.platform.qr.WutsiQrApi
+import com.wutsi.platform.qr.dto.EncodeQRCodeResponse
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.mock.mockito.MockBean
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class ProfileControllerTest : SeleniumTestSupport() {
     @MockBean
     private lateinit var accountApi: WutsiAccountApi
 
+    @MockBean
+    private lateinit var qrApi: WutsiQrApi
+
     @Test
     fun `user profile`() {
         // GIVEN
-        val account = Account(
-            id = 1,
-            displayName = "Ray Sponsible",
-            biography = "This is a sample biography of a user",
-            country = "CM",
-            language = "en",
-            pictureUrl = "https://st2.depositphotos.com/1001030/12469/i/950/depositphotos_124693804-stock-photo-afro-american-man-posing-in.jpg"
-        )
+        val account = createAccount()
         doReturn(GetAccountResponse(account)).whenever(accountApi).getAccount(any())
 
         // WHEN
@@ -42,12 +44,12 @@ internal class ProfileControllerTest : SeleniumTestSupport() {
         assertElementAttribute(
             "head meta[property='og:image']",
             "content",
-            "http://localhost:$port/qr-code/account/${account.id}.png"
+            "http://localhost:$port/profile/qr-code/${account.id}.png"
         )
 
         assertElementText(".slide--headline h1", account.displayName!!)
         assertElementText(".slide--bio", account.biography!!)
-        assertElementAttributeEndsWith("img.qr-code", "src", "/qr-code/account/${account.id}.png")
+        assertElementAttributeEndsWith("img.qr-code", "src", "/profile/qr-code/${account.id}.png")
 
         assertElementAttribute(
             ".cta-android",
@@ -56,4 +58,31 @@ internal class ProfileControllerTest : SeleniumTestSupport() {
         )
         assertElementNotPresent(".cta-ios")
     }
+
+    @Test
+    fun `qr-code`() {
+        // GIVEN
+        val account = createAccount()
+        doReturn(GetAccountResponse(account)).whenever(accountApi).getAccount(any())
+
+        doReturn(EncodeQRCodeResponse(token = "xxxxxx")).whenever(qrApi).encode(any())
+
+        // WHEN
+        val cnn = URL("http://localhost:$port/profile/qr-code/${account.id}.png").openConnection()
+        try {
+            assertTrue(cnn.getHeaderField("Content-Length").toLong() > 0)
+            assertEquals("image/png", cnn.getHeaderField("Content-Type"))
+        } finally {
+            (cnn as HttpURLConnection).disconnect()
+        }
+    }
+
+    private fun createAccount() = Account(
+        id = 1,
+        displayName = "Ray Sponsible",
+        biography = "This is a sample biography of a user",
+        country = "CM",
+        language = "en",
+        pictureUrl = "https://st2.depositphotos.com/1001030/12469/i/950/depositphotos_124693804-stock-photo-afro-american-man-posing-in.jpg"
+    )
 }
