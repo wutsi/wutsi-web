3d1479bfe4da7aca0f0d63aec8380677176fb786
Herve Tchepannou <herve@wutsi.com>
2022-05-08 01:58:10 -0400

    add tracing context header

--- src/main/kotlin/com/wutsi/application/web/endpoint/FacebookFeedController.kt
+++ src/main/kotlin/com/wutsi/application/web/endpoint/FacebookFeedController.kt
@@ -2,6 +2,7 @@ package com.wutsi.application.web.endpoint
 
 import com.amazonaws.util.IOUtils
 import com.wutsi.application.web.service.WebTokenProvider
+import com.wutsi.platform.core.tracing.TracingContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,7 +13,10 @@ import java.net.URL
 import javax.servlet.http.HttpServletResponse
 
 @RestController
-class FacebookFeedController(private val tokenProvider: WebTokenProvider) {
+class FacebookFeedController(
+    private val tokenProvider: WebTokenProvider,
+    private val traceContext: TracingContext
+) {
     @Autowired
     @Qualifier("CatalogEnvironment")
     private lateinit var env: com.wutsi.ecommerce.catalog.Environment
@@ -26,6 +30,11 @@ class FacebookFeedController(private val tokenProvider: WebTokenProvider) {
         val cnn = url.openConnection() as HttpURLConnection
         try {
             cnn.setRequestProperty("Authorization", "Bearer ${tokenProvider.getToken()}")
+            cnn.setRequestProperty(TracingContext.HEADER_TENANT_ID, traceContext.tenantId())
+            cnn.setRequestProperty(TracingContext.HEADER_CLIENT_INFO, traceContext.clientInfo())
+            cnn.setRequestProperty(TracingContext.HEADER_CLIENT_ID, traceContext.clientId())
+            cnn.setRequestProperty(TracingContext.HEADER_TRACE_ID, traceContext.traceId())
+            cnn.setRequestProperty(TracingContext.HEADER_DEVICE_ID, traceContext.deviceId())
             IOUtils.copy(cnn.inputStream, response.outputStream)
         } finally {
             cnn.disconnect()
